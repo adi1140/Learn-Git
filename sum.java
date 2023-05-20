47f0ea604112f098985259e8e245469fd09a76b0
Aditya Khote <21csme004aditya.gmail.com>
2023-05-20 10:20:46 +0530

    all files commit

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum {
     public static void main(String[] args) {
         int a = 6 , b = 12;
-        System.out.print(a+b);
+        System.out.print("This is sum : " + (a+b));
     }
 }
